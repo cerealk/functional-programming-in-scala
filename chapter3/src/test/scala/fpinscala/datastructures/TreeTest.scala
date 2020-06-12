5f2e9aac365e78dbd25289248351d64f701d4deb
cereal killer <granaglia@gmail.com>
2020-06-12 09:14:55 +0200

    finished chapter 3

--- chapter3/src/test/scala/fpinscala/datastructures/TreeTest.scala
+++ chapter3/src/test/scala/fpinscala/datastructures/TreeTest.scala
@@ -1,7 +1,7 @@
 package fpinscala.datastructures
 
-import minitest.SimpleTestSuite
 import fpinscala.datastructures.Tree._
+import minitest.SimpleTestSuite
 
 object TreeTest extends SimpleTestSuite {
 
@@ -13,25 +13,66 @@ object TreeTest extends SimpleTestSuite {
   test("maximum element") {
     val tree = Branch(Branch(Leaf(1), Leaf(2)), Leaf(3))
 
-    def maximum(t:Tree[Int]): Int = t match {
+    def maximum(t: Tree[Int]): Int = t match {
       case Leaf(v) => v
-      case Branch(l,r) => maximum(l) max maximum(r)
+      case Branch(l, r) => maximum(l) max maximum(r)
     }
 
     assertEquals(maximum(tree), 3)
   }
 
-  test ("depth") {
+  test("depth") {
     val tree = Branch(Branch(Leaf(1), Leaf(2)), Leaf(3))
 
     assertEquals(depth(tree), 3)
   }
 
-  test("map"){
+  test("map") {
     val t = Branch(Branch(Leaf(1), Leaf(2)), Leaf(3))
     val expected = Branch(Branch(Leaf("1"), Leaf("2")), Leaf("3"))
 
-    assertEquals(map(t)(String.valueOf(_)), expected)
+    assertEquals(map(t)(String.valueOf), expected)
 
   }
+
+  test("fold") {
+    val t = Branch(Branch(Leaf(1), Leaf(2)), Leaf(3))
+    assertEquals(fold(t)(a => a)(_ + _), 6)
+  }
+
+  test("count tree size via Fold") {
+    val tree = Branch(Branch(Leaf(1), Leaf(2)), Leaf(3))
+
+    def sizeF[A](t: Tree[A]) = fold(t)(_ => 1)(1 + _ + _)
+
+    assertEquals(sizeF(tree), 5)
+  }
+
+  test("maximum element via fold") {
+    val tree = Branch(Branch(Leaf(1), Leaf(2)), Leaf(3))
+
+    def maximum(t: Tree[Int]): Int = fold(t)(a => a)(_ max _)
+
+    assertEquals(maximum(tree), 3)
+  }
+
+  test("depth via fold") {
+    val tree = Branch(Branch(Leaf(1), Leaf(2)), Leaf(3))
+
+    def depthF[A](t: Tree[A]) = fold(t)(_ => 1)((l, r) => 1 + (l max r))
+
+    assertEquals(depthF(tree), 3)
+  }
+
+  test("map via fold") {
+    val t = Branch(Branch(Leaf(1), Leaf(2)), Leaf(3))
+    val expected = Branch(Branch(Leaf("1"), Leaf("2")), Leaf("3"))
+
+    def mapF[A, B](t: Tree[A])(f: A => B): Tree[B] =
+      fold(t)(a=> Leaf(f(a)):Tree[B])(Branch(_, _))
+
+    assertEquals(mapF(t)(String.valueOf), expected)
+
+  }
+
 }
